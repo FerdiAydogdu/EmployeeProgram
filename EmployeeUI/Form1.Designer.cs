0fc4fcb4acc28ccd53f09ca75588a687e99fd7a5
FerdiAydogdu <aydogduferdi14@gmail.com>
2025-11-17 22:40:35 +0300

    Add project files.

---
+++ EmployeeUI/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EmployeeUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            btnEmployee = new Button();
+            tabPage2 = new TabPage();
+            button2 = new Button();
+            btnDepartment = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(929, 490);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnDepartment);
+            tabPage1.Controls.Add(btnEmployee);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(921, 457);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Kayıt";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnEmployee
+            // 
+            btnEmployee.Location = new Point(6, 6);
+            btnEmployee.Name = "btnEmployee";
+            btnEmployee.Size = new Size(150, 30);
+            btnEmployee.TabIndex = 1;
+            btnEmployee.Text = "Personel Listesi";
+            btnEmployee.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button2);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(721, 379);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(6, 6);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // btnDepartment
+            // 
+            btnDepartment.Location = new Point(6, 42);
+            btnDepartment.Name = "btnDepartment";
+            btnDepartment.Size = new Size(150, 30);
+            btnDepartment.TabIndex = 1;
+            btnDepartment.Text = "Personel Bölümleri";
+            btnDepartment.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1038, 550);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Personel Takip Formu";
+            Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Button btnEmployee;
+        private Button button2;
+        private Button btnDepartment;
+    }
+}
