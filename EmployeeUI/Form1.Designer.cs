763764d32a2d93f9f550a016b2b027d84ead8789
FerdiAydogdu <aydogduferdi14@gmail.com>
2025-11-18 23:15:35 +0300

    *

--- EmployeeUI/Form1.Designer.cs
+++ EmployeeUI/Form1.Designer.cs
@@ -30,23 +30,29 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            btnDepartment = new Button();
             btnEmployee = new Button();
             tabPage2 = new TabPage();
             button2 = new Button();
-            btnDepartment = new Button();
+            panelMenu = new Panel();
+            button3 = new Button();
+            button1 = new Button();
+            btnMainEmployee = new Button();
+            panelLogo = new Panel();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             tabPage2.SuspendLayout();
+            panelMenu.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
             // 
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(12, 12);
+            tabControl1.Location = new Point(730, 307);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(929, 490);
+            tabControl1.Size = new Size(455, 181);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -56,10 +62,19 @@
             tabPage1.Location = new Point(4, 29);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(921, 457);
+            tabPage1.Size = new Size(447, 148);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Kayıt";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnDepartment
+            // 
+            btnDepartment.Location = new Point(6, 42);
+            btnDepartment.Name = "btnDepartment";
+            btnDepartment.Size = new Size(150, 30);
+            btnDepartment.TabIndex = 1;
+            btnDepartment.Text = "Personel Bölümleri";
+            btnDepartment.UseVisualStyleBackColor = true;
             // 
             // btnEmployee
             // 
@@ -76,7 +91,7 @@
             tabPage2.Location = new Point(4, 29);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(721, 379);
+            tabPage2.Size = new Size(447, 148);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
@@ -90,28 +105,99 @@
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
             // 
-            // btnDepartment
+            // panelMenu
             // 
-            btnDepartment.Location = new Point(6, 42);
-            btnDepartment.Name = "btnDepartment";
-            btnDepartment.Size = new Size(150, 30);
-            btnDepartment.TabIndex = 1;
-            btnDepartment.Text = "Personel Bölümleri";
-            btnDepartment.UseVisualStyleBackColor = true;
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Controls.Add(btnMainEmployee);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 550);
+            panelMenu.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.Gainsboro;
+            button3.Image = Properties.Resources.Employee;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 200);
+            button3.Name = "button3";
+            button3.Padding = new Padding(12, 0, 0, 0);
+            button3.Size = new Size(220, 60);
+            button3.TabIndex = 3;
+            button3.Text = "Employee";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.Gainsboro;
+            button1.Image = Properties.Resources.Employee;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(0, 140);
+            button1.Name = "button1";
+            button1.Padding = new Padding(12, 0, 0, 0);
+            button1.Size = new Size(220, 60);
+            button1.TabIndex = 2;
+            button1.Text = "Employee";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // btnMainEmployee
+            // 
+            btnMainEmployee.Dock = DockStyle.Top;
+            btnMainEmployee.FlatAppearance.BorderSize = 0;
+            btnMainEmployee.FlatStyle = FlatStyle.Flat;
+            btnMainEmployee.ForeColor = Color.Gainsboro;
+            btnMainEmployee.Image = Properties.Resources.Employee;
+            btnMainEmployee.ImageAlign = ContentAlignment.MiddleLeft;
+            btnMainEmployee.Location = new Point(0, 80);
+            btnMainEmployee.Name = "btnMainEmployee";
+            btnMainEmployee.Padding = new Padding(12, 0, 0, 0);
+            btnMainEmployee.Size = new Size(220, 60);
+            btnMainEmployee.TabIndex = 1;
+            btnMainEmployee.Text = "Employee";
+            btnMainEmployee.TextAlign = ContentAlignment.MiddleLeft;
+            btnMainEmployee.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnMainEmployee.UseVisualStyleBackColor = true;
+            btnMainEmployee.Click += btnMainEmployee_Click;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(220, 80);
+            panelLogo.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1038, 550);
+            ClientSize = new Size(1235, 550);
+            Controls.Add(panelMenu);
             Controls.Add(tabControl1);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Personel Takip Formu";
-            Load += Form1_Load;
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage2.ResumeLayout(false);
+            panelMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -123,5 +209,10 @@
         private Button btnEmployee;
         private Button button2;
         private Button btnDepartment;
+        private Panel panelMenu;
+        private Panel panelLogo;
+        private Button btnMainEmployee;
+        private Button button3;
+        private Button button1;
     }
 }
--- EmployeeUI/Form1.cs
+++ EmployeeUI/Form1.cs
@@ -1,15 +1,79 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+
 namespace EmployeeUI
 {
     public partial class Form1 : Form
     {
+        // 10:24
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private Color SelectThemeColor()
         {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
 
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    //panelTitleBar.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    //btnCloseChildForm.Visible = true;
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void btnMainEmployee_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
 }
